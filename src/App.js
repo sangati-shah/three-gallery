4bf9ffe63582b3510b83599dc45ee9d8b8f51385
sangati-shah <sash7814@colorado.edu>
2025-11-02 20:14:40 -0600

    Initial commit of three-gallery project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import InteractiveGallery from './InteractiveGallery';
+
+function App() {
+  return <InteractiveGallery />;
+}
+
+export default App;
---
+++ src/galleryData.js
@@ -0,0 +1,49 @@
+// src/galleryData.js
+export const galleryData = [
+  {
+    name: "Watching Waves",
+    images: [
+      { path: '/gallery/WatchingWaves/crash.jpg', name: 'Crash' },
+      { path: '/gallery/WatchingWaves/erosion.jpg', name: 'Erosion' },
+      { path: '/gallery/WatchingWaves/fall.jpg', name: 'Fall' },
+      { path: '/gallery/WatchingWaves/floating.jpg', name: 'Floating' },
+      { path: '/gallery/WatchingWaves/ice.jpg', name: 'Ice' },
+      { path: '/gallery/WatchingWaves/octo.jpg', name: 'Octopussy' },
+      { path: '/gallery/WatchingWaves/reflection.jpg', name: 'Reflection' },
+      { path: '/gallery/WatchingWaves/greatWave.jpg', name: 'Great Wave' },
+      { path: '/gallery/WatchingWaves/niagaraFalls.jpg', name: 'Niagara Falls' },
+      { path: '/gallery/WatchingWaves/mountains.jpg', name: 'Mountain Range' },
+    ]
+  },
+  {
+    name: "Coloring Book",
+    images: [
+      { path: '/gallery/ColoringBook/hydrangeas.jpg', name: 'Hydrangeas' },
+      { path: '/gallery/ColoringBook/jellyfishes.jpg', name: 'Jellyfishes' },
+      { path: '/gallery/ColoringBook/lalQila.jpg', name: 'Lal Qila' },
+      { path: '/gallery/ColoringBook/lotus.jpg', name: 'Lotus' },
+      { path: '/gallery/ColoringBook/Owl.jpg', name: 'Owl' },
+      { path: '/gallery/ColoringBook/rose.jpg', name: 'Rose' },
+      { path: '/gallery/ColoringBook/sara.jpg', name: 'Girl' },
+      { path: '/gallery/ColoringBook/violet.jpg', name: 'Violet' },
+      { path: '/gallery/ColoringBook/cherryBlossoms.jpg', name: 'Cherry Blossoms' },
+      { path: '/gallery/ColoringBook/love.jpg', name: 'Best Friends' },
+    ]
+  },
+  {
+    name: "That Which Remains",
+    images: [
+      { path: '/gallery/Misc/aspen.jpg', name: 'Aspen' },
+      { path: '/gallery/Misc/bird.jpg', name: 'Bird' },
+      { path: '/gallery/Misc/crazy.jpg', name: 'Crazy' },
+      { path: '/gallery/Misc/eternalDance.jpg', name: 'Eternal Dance' },
+      { path: '/gallery/Misc/heart.jpg', name: 'Heart' },
+      { path: '/gallery/Misc/mangoes.jpg', name: 'Mangoes' },
+      { path: '/gallery/Misc/mural.jpg', name: 'Mural' },
+      { path: '/gallery/Misc/chick.jpg', name: 'Pretty Woman' },
+      { path: '/gallery/Misc/old.jpg', name: 'Old' },
+      { path: '/gallery/Misc/sumo.jpg', name: 'Sumo Wrestler' },
+      { path: '/gallery/Misc/vase.jpg', name: 'Vase' },
+    ]
+  }
+];
